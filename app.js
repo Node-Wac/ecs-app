fe5e6cdbf65b918541b12979cc57cfb9da7ec51e
Aslam <aslam@webandcrafts.com>
2024-09-23 17:25:11 +0530

    express app added

---
+++ app.js
@@ -0,0 +1,14 @@
+// app.js
+const express = require("express");
+const app = express();
+
+// Define a simple route for the home page
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+// Start the server and listen on port 3000
+const port = 4000;
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
